4863bcf23f07a53513c066093ba4814d6eb200b3
Albin Nilsson <c22ann@cs.umu.se>
2025-06-19 16:38:16 +0200

    added base test to check pipeline

---
+++ backend/tests/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Base Express App', () => {
+  it('GET / should return 200 OK', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('Unknown routes return 404', async () => {
+    const res = await request(app).get('/no-such-route');
+    expect(res.statusCode).toBe(404);
+  });
+});
