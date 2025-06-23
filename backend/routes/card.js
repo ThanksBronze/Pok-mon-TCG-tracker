fc99e1c6c5b5abfcbd7e55000d2ee0505aba80b0
Albin Nilsson <c22ann@cs.umu.se>
2025-06-23 11:59:37 +0200

    Created GET for individual card

--- backend/routes/card.js
+++ backend/routes/card.js
@@ -6,10 +6,22 @@ const router = express.Router();
 // GET /api/cards
 router.get('/', async(req, res, next) => {
 	try{
-		const{ rows } = await pool.query(
+		const{rows} = await pool.query(
 			'SELECT * FROM cards WHERE user_id = $1 AND (deleted_at IS NULL)',
 			[req.user.id]
 		);
 		res.json(rows);
 	} catch (err) { next(err); }
+});
+
+//GET /api/cards/:id
+router.get('/:id', async(req, res, next) => {
+	try{
+		const {rows} = await pool.query(
+			'SELECT * FROM cards WHERE id=$1 AND user_id=$2 AND (deleted_at IS NULL)',
+			[req.params.id, req.user.id]
+		);
+		if(!rows.length) return res.status(404).json({message: 'Kortet finns inte'});
+		res.json(rows[0]);
+	} catch(err) {next(err);}
 });
