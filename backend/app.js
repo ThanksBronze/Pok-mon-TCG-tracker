4b3ac2ffd34eb3642a461fa9eb456e3e4bd752f1
Albin Nilsson <c22ann@cs.umu.se>
2025-07-03 21:56:59 +0200

    Added new API:s to app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const setsRouter = require('./routes/sets');
+const cardTypesRouter = require('./routes/card_types');
+const seriesRouter = require('./routes/series');
 
 var app = express();
 
@@ -21,9 +24,20 @@ app.use((req, res, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 console.log('Mounting cardsRouter on /api/cards');
 app.use('/api/cards', cardsRouter);
+
+console.log('Mounting setsRouter on /api/sets');
+app.use('/api/sets', setsRouter);
+
+console.log('Mounting cardTypesRouter on /api/card_types');
+app.use('/api/card-types', cardTypesRouter);
+
+console.log('Mounting seriesRouter on /api/series');
+app.use('/api/series', seriesRouter);
+
+console.log('Mounting usersRouter on /api/users');
+app.use('/api/users', usersRouter);
 
 module.exports = app;
