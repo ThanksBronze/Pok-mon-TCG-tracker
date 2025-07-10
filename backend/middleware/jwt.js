f71c0ed45052c2b4499bfd23b8572fa2619efbf1
Albin Nilsson <c22ann@cs.umu.se>
2025-07-10 23:36:44 +0200

    Added jwt middleware

---
+++ backend/middleware/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+module.exports = function(req, res, next){
+	const auth = req.headers.authorization?.split(' ');
+	if (!auth || auth[0] !== 'Bearer') return res.status(401).json({ message: 'Ingen token' });
+	try {
+		const payload = jwt.verify(auth[1], process.env.JWT_SECRET);
+		req.user = { id: payload.sub, username: payload.username };
+		next();
+	} catch {
+		res.status(401).json({ message: 'Ogiltig token' });
+	}
+};
