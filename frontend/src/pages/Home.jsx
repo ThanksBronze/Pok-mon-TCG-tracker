968b9cb2fefe12f3921318d20b0005f667914ec7
Albin Nilsson <c22ann@cs.umu.se>
2025-07-26 00:06:10 +0200

    Updated homepage to add card to list

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchCards as apiFetchCards } from '../api/cards';
+import CardForm from '../components/CardForm';
 import CardList from '../components/CardList';
 
-function Home() {
-	return <CardList />;
-}
+export default function Home() {
+	const [cards, setCards] = useState([]);
 
-export default Home;
+	useEffect(() => {
+		apiFetchCards()
+			.then(res => setCards(res.data))
+			.catch(err => console.error('Fetch cards error:', err));
+	}, []);
+
+	const handleNewCard = newCard => {
+		setCards(cs => [newCard, ...cs]);
+	};
+
+	return (
+		<div className="manage-cards">
+			<CardForm onSuccess={handleNewCard} />
+			<CardList cards={cards} />
+		</div>
+	);
+}
